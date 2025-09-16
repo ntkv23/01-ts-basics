971b2ee66867b19d0a4724f715c229cf2f3219f5
aliaksei <ntkv2311@gmail.com>
2025-09-16 16:40:12 +0200

    hw1

---
+++ src/task-5.ts
@@ -0,0 +1,19 @@
+type status = "loading" | "success" | "error";
+function logStatus(status: status) {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
+
+// Завдання:
+
+// Типізуй параметр status так, щоб дозволити тільки три конкретні рядкові значення: "loading", "success", "error"
+// Переконайся, що TypeScript не дозволяє передати будь-яке інше значення.
+// Типізуй повернення функції, зауваж що вона нічого явно не повертає.
+// Залиш реалізацію функції без змін.
